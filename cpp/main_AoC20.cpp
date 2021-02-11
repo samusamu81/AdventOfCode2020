2bfe8736bea133fa0686e280e193f5aeb5bb51c5
Samuele Sommariva <samusamu81@yahoo.it>
2021-02-11 05:15:37 +0100

    Cleaning and folder managment

--- cpp/main_AoC20.cpp
+++ cpp/main_AoC20.cpp
@@ -112,6 +112,7 @@ int main(int argc, char* argv[])
     myAnsw = AoC24_20();
     std::cout << myAnsw.first << "\t" << myAnsw.second << std::endl << std::endl;
 
+
     return 0;
 }
 
---
+++ hpp/AoC20.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+
+#include<vector> // for iterators 
+#include<tuple>
+#include<string>
+#include<algorithm>
+#include<deque>
+
+
+
+std::pair<size_t, size_t> AoC01_20(std::string myFileName = ".\\inp\\Test1_20.txt");
+std::pair<size_t, size_t> AoC02_20(std::string myFileName = ".\\inp\\Test2_20.txt");
+std::pair<size_t, size_t> AoC03_20(std::string myFileName = ".\\inp\\Test3_20.txt");
+std::pair<size_t, size_t> AoC04_20(std::string myFileName = ".\\inp\\Test4_20.txt");
+std::pair<size_t, size_t> AoC05_20(std::string myFileName = ".\\inp\\Test5_20.txt");
+std::pair<size_t, size_t> AoC06_20(std::string myFileName = ".\\inp\\Test6_20.txt");
+std::pair<size_t, size_t> AoC07_20(std::string myFileName = ".\\inp\\Test7_20.txt");
+std::pair<size_t, size_t> AoC08_20(std::string myFileName = ".\\inp\\Test8_20.txt");
+std::pair<size_t, size_t> AoC09_20(std::string myFileName = ".\\inp\\Test9_20.txt");
+std::pair<size_t, size_t> AoC10_20(std::string myFileName = ".\\inp\\Test10_20.txt");
+std::pair<size_t, size_t> AoC11_20(std::string myFileName = ".\\inp\\Test11_20.txt");
+std::pair<size_t, size_t> AoC12_20(std::string myFileName = ".\\inp\\Test12_20.txt");
+std::pair<size_t, size_t> AoC13_20(std::string myFileName = ".\\inp\\Test13_20.txt");
+std::pair<size_t, size_t> AoC14_20(std::string myFileName = ".\\inp\\Test14_20.txt");
+std::pair<size_t, size_t> AoC15_20(std::string myFileName = ".\\inp\\Test15_20.txt");
+std::pair<size_t, size_t> AoC16_20(std::string myFileName = ".\\inp\\Test16_20.txt");
+std::pair<size_t, size_t> AoC17_20(std::string myFileName = ".\\inp\\Test17_20.txt");
+std::pair<size_t, size_t> AoC18_20(std::string myFileName = ".\\inp\\Test18_20.txt");
+std::pair<size_t, size_t> AoC19_20(std::string myFileName = ".\\inp\\Test19_20.txt");
+std::pair<size_t, size_t> AoC20_20(std::string myFileName = ".\\inp\\Test20_20.txt");
+std::pair<size_t, std::string> AoC21_20(std::string myFileName = ".\\inp\\Test21_20.txt");
+std::pair<size_t, size_t> AoC22_20(std::string myFileName = ".\\inp\\Test22_20.txt");
+std::pair<size_t, size_t> AoC23_20(std::string myInput = "193467258");
+std::pair<size_t, size_t> AoC24_20(std::string myFileName = ".\\inp\\Test24_20.txt");
+std::pair<size_t, size_t> AoC25_20(int pub1 = 1717001, int pub2 = 523731);
